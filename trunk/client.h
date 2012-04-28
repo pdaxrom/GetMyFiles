aa603a84b11d321902273ed8d0b01b72e9648d46
sash <sash@519c0268-f812-47ac-b17b-35f1d451d457>
2012-04-28 13:19:37 +0000

    update client

--- trunk/client.h
+++ trunk/client.h
@@ -5,12 +5,13 @@
 extern "C" {
 #endif
 
+#ifdef CLIENT_GUI
 void show_server_directory(char *str);
+void update_client(char *vers);
+#endif
 
 int client_connect(char *_host, int _port, char *_root_dir, int *_sock);
-
 void client_disconnect(int _sock);
-
 
 #ifdef __cplusplus
 }
