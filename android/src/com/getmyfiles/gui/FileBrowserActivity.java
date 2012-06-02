f06bcf679132deaab772675ed35c256b463760e7
sash <sash@519c0268-f812-47ac-b17b-35f1d451d457>
2012-06-02 07:12:59 +0000

    select path from filebrowser

--- android/src/com/getmyfiles/gui/FileBrowserActivity.java
+++ android/src/com/getmyfiles/gui/FileBrowserActivity.java
@@ -4,13 +4,18 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -19,12 +24,24 @@ public class FileBrowserActivity extends ListActivity {
 	private List<String> path = null;
 	private String root="/sdcard";
 	private TextView myPath;
+	private Button okButton;
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.filebrowser);
+        okButton = (Button) findViewById(R.id.okButton);
+        okButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+            	String path = myPath.getText().toString();
+            	Log.i("filebrowser", path);
+            	Intent intent = new Intent();
+            	intent.putExtra("path", path);
+            	setResult(Activity.RESULT_OK, intent);
+            	finish();
+            }
+        });
         myPath = (TextView)findViewById(R.id.pathText);
         getDir(root);
     }
