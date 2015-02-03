2046dd0aa21e1ede08b38f8265f0e09bb95d8f32
sash <sash@519c0268-f812-47ac-b17b-35f1d451d457>
2015-02-03 08:04:06 +0000

    openssl updated

--- trunk/android/jni/Application.mk
+++ trunk/android/jni/Application.mk
@@ -1,5 +1,6 @@
 APP_PLATFORM	:= android-9
-APP_ABI		:= mips armeabi armeabi-v7a x86
+APP_ABI		:= armeabi 
+#armeabi-v7a mips x86
 
 # Uncomment this if you're using STL in your project
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
